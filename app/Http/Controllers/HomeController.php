ffcbc1c59cc67b967039bc49a02e2c00f338d8fa
maful <mafulprayoga@gmail.com>
2019-02-09 15:08:11 +0700

    displays the last 5 mutations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SavingHistory;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $mutations = SavingHistory::whereDate('created_at', \Carbon\Carbon::today())
+                                    ->take(5)
+                                    ->latest()
+                                    ->get();
+
+        return view('home', compact('mutations'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,45 @@
 @section('page-title', 'Dashboard')
 
 @section('content-app')
-{{-- <div class="row row-cards">
-    <div class="col-6 col-sm-4 col-lg-2">
+<div class="row row-cards">
+    <div class="col-12">
         <div class="card">
-            <div class="card-body p-3 text-center">
-                <div class="text-right text-green">
-                    6%
-                    <i class="fe fe-chevron-up"></i>
-                </div>
-                <div class="h1 m-0">43</div>
-                <div class="text-muted mb-4">New Tickets</div>
+            <div class="card-header">
+                <h3 class="card-title">5 Mutasi Terbaru Hari Ini</h3>
+            </div>
+            <div class="table-responsive">
+                <table class="table card-table table-vcenter text-nowrap datatable">
+                    <thead>
+                        <tr>
+                            <th class="w-1">No.</th>
+                            <th>Tanggal</th>
+                            <th>Keterangan</th>
+                            <th class="text-right">Debet</th>
+                            <th class="text-right">Kredit</th>
+                            <th class="text-right">Saldo</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($mutations as $mutation)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $mutation->tanggal }}</td>
+                                <td>{{ $mutation->keterangan }}</td>
+                                <td class="text-right">{{ format_rupiah($mutation->debet) }}</td>
+                                <td class="text-right">{{ format_rupiah($mutation->kredit) }}</td>
+                                <td class="text-right">{{ format_rupiah($mutation->saldo) }}</td>
+                                <td>{{ $mutation->created_at->diffForHumans() }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7">Data tidak ditemukan.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-</div> --}}
+</div>
 @endsection
