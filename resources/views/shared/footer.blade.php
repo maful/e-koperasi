adb2e6ba36592f6fbbff4a559a03e26493aba653
maful <mafulprayoga@gmail.com>
2020-02-08 12:02:20 +0700

    prevent the original page from being modified by the opened link.

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="col-auto ml-lg-auto">
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0 text-center">
-                Copyright © 2019 <a href=".">E-Koperasi</a>. Theme by <a href="https://codecalm.net" target="_blank">codecalm.net</a> All rights reserved.
+                Copyright © 2019 <a href=".">E-Koperasi</a>. Theme by <a href="https://codecalm.net" target="_blank" rel="noopener noreferrer">codecalm.net</a> All rights reserved.
             </div>
         </div>
     </div>
